c52a92ab5c1054242884119db96213d3eb36110e
gireesh27 <136105197+gireesh27@users.noreply.github.com>
2025-03-04 17:27:58 +0530

    removed floating navbar

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,6 @@ import { FaHome } from "react-icons/fa";
 export default function Home() {
   return (
     <main className="relative bg-black-100 text-white flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10  px-5">
-      <div className="max-w-7xl w-full">
-        <FloatingNav
-        className=""
-        navItems={[
-          {name:"home",link:"/",icon:<FaHome/>}
-        ]}/>
        <Hero/>
        <Grid/>
       </div>
